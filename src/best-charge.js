e6f97e6180f7e3de4e6ab9404c990a14d6019c12
owen git <64907376@qq.com>
2019-07-10 23:23:18 +0800

    The function of getting the serial number and quantity of the dish is completed.

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,13 @@
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+function getItemAndCount(selectedItems){
+    var order = selectedItems.map((vaule) =>{
+       var product ={
+             item:vaule.split("x")[0],
+             count:vaule.split("x")[1]
+       };
+       return product;
+       })
+    return order;
+}
