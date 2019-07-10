38b2562747df61cbe56b080e5e1437ccc1414261
owen git <64907376@qq.com>
2019-07-10 23:55:37 +0800

    The second calculation discount function is completed

--- src/best-charge.js
+++ src/best-charge.js
@@ -4,10 +4,36 @@ function bestCharge(selectedItems) {
 function getItemAndCount(selectedItems){
     var order = selectedItems.map((vaule) =>{
        var product ={
-             item:vaule.split("x")[0],
+             id:vaule.split("x")[0],
              count:vaule.split("x")[1]
        };
        return product;
        })
     return order;
+}
+function getDiscount(order){
+    var halfDiscount = [];
+    var fullDiscount = [];
+    var allItems = loadAllItems();
+    var promote = loadPromotions();
+    halfDiscount = order.reduce((acct,value) =>{
+      acct += promote.items.includes(value.id)?allItems.find((item)=>{
+        return item.id == vaule.id
+      }).price*value.count*0.5:allItems.find((item)=>{
+        return item.id == vaule.id
+      }).price*value.count;
+      return acct
+    },0)
+    fullDiscount = order.reduce((acct,value) =>{
+      acct +=allItems.find((item)=>{
+         return item.id == vaule.id
+      }).price*value.count;
+      return acct
+    },0)
+    fullDiscount = fullDiscount>33?fullDiscount-6:fullDiscount
+    var allDiscount = {
+      halfDiscount:halfDiscount,
+      fullDiscount:fullDiscount
+    }
+    return allDiscount;
 }
